b0dc1eca2058acff6a8ba6399d142f83bd211767
Ezequiel Leonardo Castaño <elcg@gmx.com>
2018-04-19 12:31:14 -0300

    :construction_worker: Use global installation for Firebase and Netlify

--- publish.sh
+++ publish.sh
@@ -5,5 +5,5 @@ git config --global user.name "Travis CI"
 git checkout -b $1
 git add .
 git commit -m "Travis Build"
-git remote add origin https://${GH_TOKEN}@github.com/ELC/hello-angular.git
+git remote add origin https://${GH_TOKEN}@github.com/ELC/hello-angular.git > /dev/null 2>&1
 git push --force --quiet --set-upstream origin $1
