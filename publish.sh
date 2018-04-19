a4f26901b0fc86df31fd91810d2fd10b0f2eb694
Ezequiel Leonardo Castaño <elcg@gmx.com>
2018-04-19 06:25:21 -0300

    :green_heart: Fix publish.sh

--- publish.sh
+++ publish.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
 git init
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
-git checkout -b $0
+git checkout -b $1
 git add .
 git commit -m "Travis Build"
-git remote add origin https://${GH_TOKEN}@github.com/ELC/hello-angular.git > /dev/null 2>&1
-git push --force --quiet --set-upstream origin $0
+git remote add origin https://${GH_TOKEN}@github.com/ELC/hello-angular.git
+git push --force --quiet --set-upstream origin $1
